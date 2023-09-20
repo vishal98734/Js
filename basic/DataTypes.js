5e34b50c7c9b746a3a0c046676258fcbc7d0c0f2
Vishal Singh <129960273+vishal98734@users.noreply.github.com>
2023-09-20 14:28:26 +0000

    Data type

---
+++ basic/DataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer verison
+
+// alert (3+3) // we are using node js,not browser
+
+console.log("vishal")
+
+let name = "vishal"
+let age = 20
+let isloggedIn = false
+
+// number => 2 to power 53
+/// bigint
+// string => ""
+// boolean => standalone Value
+// undefined =>
+// symbol => unique
+
+//object
+
+console.log(typeof undefined); //undefined
+
+console.log(typeof null); //object
