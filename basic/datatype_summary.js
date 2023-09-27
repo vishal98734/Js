b253d90163865eba7e88252c2ab20005a2abba39
Vishal Singh <129960273+vishal98734@users.noreply.github.com>
2023-09-27 12:26:17 +0000

    summary

---
+++ basic/datatype_summary.js
@@ -0,0 +1,31 @@
+// Primtive
+
+// 7 types : String, Number, Boolearn, undefined, symbol, BigInt
+
+const score = 100
+const scoreValue = 100
+
+const isloggedIn = 100.3
+const outsideTemp = null
+let userEmail;
+
+const Id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(Id === anotherId);
+
+// const bigNumber = 253654266525n
+// Reference (Non Primtive)
+
+// Array, Objects, Function
+
+const heros = ["shaktiman", "naagraj", "doga"]
+ let myObj = {
+    name: "vishal"
+}
+
+const myFunction = function(){
+    console.log("hello world")
+}
+
+console.log(typeof outsideTemp);
