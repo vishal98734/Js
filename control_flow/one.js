195356271535aa3d187a6bf59e65e41b5bf72058
Vishal Singh <129960273+vishal98734@users.noreply.github.com>
2023-10-09 13:20:52 +0000

    flow

---
+++ control_flow/one.js
@@ -0,0 +1,48 @@
+// if
+
+//   < less then
+//   > greater then
+//   <= less then equal
+//   >= greater then equal
+//   == for checking equal
+//   != not equal
+//   === Strict equality for checking data  type
+//   !==
+
+
+
+const isUserloggedIn = true
+const temperture = 41
+if (temperture === 40) {
+   console.log("less than 50");
+} else {
+   console.log("temperture is greater then 50");
+} 
+//console.log("execute");   
+
+// const score = 200
+// if (score > 100){
+//    const power = "fly"
+//    console.log(`User power: ${power}`);
+// }
+// console.log(`User power: ${power}`);
+
+const bal = 1000
+// if (bal > 500) console.log("test");
+
+// if (bal < 500) {
+//    console.log("less then 500 ");
+// } else if (bal < 750){
+//    console.log("less then 750");
+// } else if (bal < 900){
+//    console.log("less then 750");
+// } else{
+//    console.log("less then 1200");
+// }
+
+const UserloggedIn = true
+const debitCard = true
+
+if (UserloggedIn && debitCard && 2==3 ) {
+   console.log("allow to buy course");  
+}
